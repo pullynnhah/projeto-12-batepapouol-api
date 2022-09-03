c8a0f57abe5e9b9f5f677a0b272d3f745b0b991f
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-03 19:36:58 -0300

    create schemas for validation

---
+++ utils/schemas.js
@@ -0,0 +1,13 @@
+import joi from "joi";
+
+const participantSchema = joi.object({
+  name: joi.string().required(),
+});
+
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
+});
+
+export {participantSchema, messageSchema};
